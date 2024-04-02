7bc958b6d61ad735935413956c4ee6a0a38ec9ca
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-04-02 11:11:01 +0200

    add name and img

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import MichelAngelo from "MichelAngelo";
 import './App.css'
 
 function App() {
@@ -8,7 +9,10 @@ function App() {
 
   return (
     <>
-     
+     <MichelAngelo 
+     img= ""
+     name="Michelangelo"
+     />
     </>
   )
 }
