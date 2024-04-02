07c1c1c092a2a6e51a3d5bf11f30b36e9b909d8d
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-04-02 11:09:19 +0200

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TurtleCard from './components/donatello'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/components/Donatello.jsx
@@ -0,0 +1,6 @@
+
+function TurtleCard() {
+
+}
+
+export default TurtleCard
