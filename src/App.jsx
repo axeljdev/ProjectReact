b26bdd2e0c7ae4a49d7d4c3b2b230aae5634d4eb
ALEXANDRE MORO <alexandre.moro@live.fr>
2024-04-02 16:33:37 +0200

    rename component card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import RaphaeloCard from './components/Raphaelo';
+import RaphaeloCard from './components/TurtleCard';
 
 const ninjaTurtles =[{
   name : "Leonardo",
@@ -20,14 +20,14 @@ const ninjaTurtles =[{
 },
 ] 
 function App() {
-
   return (
     <>
      {ninjaTurtles.map((turtle) =>
      <RaphaeloCard
      key={turtle.name}
      img={turtle.img}
-     name={turtle.name}/>)}
+     name={turtle.name}
+     handLeClick={handLeClick}/>)}
     </>
   )
 }
